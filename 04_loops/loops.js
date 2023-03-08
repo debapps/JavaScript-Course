f9f7754f630f810147bb7d1df97a6de582efe6e5
Debaditya Bhar <bhar.debaditya@gmail.com>
2023-03-08 20:26:05 +0530

    The Do-while loops is added.

--- 04_loops/loops.js
+++ 04_loops/loops.js
@@ -35,13 +35,26 @@ for (let letter of message) {
   console.log(letter);
 }
 
-// While loop: Show the first 10 positive even numbers.
+// While loop: Show the positive even numbers between 0 - 10.
+// The condition is checked first, if it is true, the loop body executed.
 
-console.log("\nThe first 10 positive even numbers:");
+console.log("\nThe positive even numbers between 0 - 10:");
 let i = 0;
-while (i < 10) {
+while (i <= 10) {
   if (i % 2 == 0) {
     console.log(i);
   }
   i++;
 }
+
+// Do-While loop: Show the positive odd numbers between 0 - 10.
+// The loop body exected first, then condition is checked. If the condition is true, the body executed again.
+
+console.log("\nThe positive odd numbers between 0 - 10:");
+let j = 0;
+do {
+  if (j % 2 !== 0) {
+    console.log(j);
+  }
+  j++;
+} while (j <= 10);
--- README.md
+++ README.md
@@ -7,4 +7,4 @@ This is introductory JavaScript Course. The programs will be stored in following
 - **01_variables** - JavaScript Variables and Constants, Objects and Classes. Added the concept of hoisting.
 - **02_operators** - JavaScript Operators: Arithmatic, Increament/Decrement, Assignment, Comparision, Logical, Trinary, Type.
 - **03_conditional** - JavaScript Conditional Statements: if-else-if-else, switch-case.
-- **04_loops** - JavaScript Loop Statements: for, for in, for of, while.
+- **04_loops** - JavaScript Loop Statements: for, for in, for of, while, do-while.
